92180a7116545e1dff16a099742c36a67225cf66
monty <velimir.vesselinov@gmail.com>
2015-06-05 11:55:17 -0600

    julia fixes

--- .juliarc.jl
+++ .juliarc.jl
@@ -1,7 +1,7 @@
 push!(LOAD_PATH, ENV["HOME"]*"/.julia")
-push!(LOAD_PATH, ENV["HOME"]*"/scripts.jl")
-push!(LOAD_PATH, ENV["HOME"]*"/codes/mads.jl")
-push!(Sys.DL_LOAD_PATH, ENV["HOME"]*"/mads/repo/bin/Lib")
+#push!(LOAD_PATH, ENV["HOME"]*"/scripts.jl")
+#push!(LOAD_PATH, ENV["HOME"]*"/codes/mads.jl")
+#push!(Sys.DL_LOAD_PATH, ENV["HOME"]*"/mads/repo/bin/Lib")
 ## give a local .juliarc.jl a chance
 if chomp(readall(`pwd`)) != ENV["HOME"]
     if isfile(".juliarc.jl")
