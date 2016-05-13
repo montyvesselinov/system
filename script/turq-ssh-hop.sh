1cd1c919494d77a5e5d55f9c57b0c2b72638f290
monty <velimir.vesselinov@gmail.com>
2016-05-13 12:24:35 -0600

    gitpulldirs added

--- script/turq-ssh-hop.sh
+++ script/turq-ssh-hop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TURQ_REGEXP="^(cj|mp|ml|mu|pi|wf|ls|hb|gl|bz)-fe[1-9]?(\.lanl\.gov)?$"
 if [[ $1 =~ $TURQ_REGEXP ]]; then
-	exec ssh vvv@wtrw2 ssh "$@"
+	exec ssh vvv@wtrw2.lanl.gov ssh "$@"
 else
 	exec ssh "$@"
 fi
