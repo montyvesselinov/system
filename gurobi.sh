6a8422a010ba10a2ef362ef9601fa231b1948d6c
monty <velimir.vesselinov@gmail.com>
2017-09-02 15:42:08 -0600

    ssh added

--- gurobi.sh
+++ gurobi.sh
@@ -2,3 +2,4 @@ export GUROBI_HOME="/home/vvv/gurobi750/linux64"
 export PATH="${PATH}:${GUROBI_HOME}/bin" 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
 export GRB_LICENSE_FILE=/home/vvv/system/gurobi.lic
+ssh -L 41954:localhost:41954 vvv@license.lanlytics.com
