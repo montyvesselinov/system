5e3ca28c6fe10c08dd06ef41eaad3d2a70c89067
monty <velimir.vesselinov@gmail.com>
2017-09-02 15:57:46 -0600

    ssh fixes

--- gurobi.sh
+++ gurobi.sh
@@ -1,5 +1,8 @@
-export GUROBI_HOME="/home/vvv/gurobi750/linux64" 
+homedir=~
+eval homedir=$homedir
+echo $homedir
+export GUROBI_HOME="$homedir/gurobi750/linux64" 
 export PATH="${PATH}:${GUROBI_HOME}/bin" 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
-export GRB_LICENSE_FILE=/home/vvv/system/gurobi.lic
+export GRB_LICENSE_FILE="$homedir/system/gurobi.lic"
 ssh -L 41954:localhost:41954 vvv@license.lanlytics.com
