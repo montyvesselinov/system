222593d226fe08d0d633c15fa7749fc105728cae
monty <velimir.vesselinov@gmail.com>
2017-09-05 14:19:41 -0600

    -f -N added

--- gurobi.sh
+++ gurobi.sh
@@ -5,4 +5,4 @@ export GUROBI_HOME="$homedir/gurobi750/linux64"
 export PATH="${PATH}:${GUROBI_HOME}/bin" 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
 export GRB_LICENSE_FILE="$homedir/system/gurobi.lic"
-ssh -L 41954:localhost:41954 vvv@license.lanlytics.com
+ssh -f -N -L 41954:localhost:41954 vvv@license.lanlytics.com
