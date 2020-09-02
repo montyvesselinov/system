7a6c09f115e01b43a5e45fe52888ed4d9b48772c
monty <velimir.vesselinov@gmail.com>
2020-09-02 17:52:16 -0600

    system/update-git-completion.bash

--- update-git-completion.bash
+++ update-git-completion.bash
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O source/git-completion.bash
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
